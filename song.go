bd5cff18a4a4f121918e0fd30f1f0c2d4b4aed7d
Zibadi <zybdym@gmail.com>
2024-03-26 15:16:42 +0330

    Add loading directory songs recursively

--- song.go
+++ song.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 
 	"github.com/dhowden/tag"
 	"github.com/hajimehoshi/ebiten/v2/audio/mp3"
@@ -13,38 +15,17 @@ type Song struct {
 }
 
 func loadSongs(settings *Settings) []*Song {
-	entries := readDir(settings.dir)
-	songsPath := getSongsPath(settings.dir, entries)
-	songs := newSongs(songsPath)
-	return songs
-}
-
-func readDir(dir string) []os.DirEntry {
-	entries, err := os.ReadDir(dir)
-	if err != nil {
-		fmt.Printf("[ERROR]: %v", err)
-		os.Exit(0)
-	}
-	return entries
-}
-
-func getSongsPath(dir string, entries []os.DirEntry) []string {
-	paths := []string{}
-	for _, e := range entries {
-		// TODO: Read songs of the sub directories too
-		if !e.IsDir() {
-			fileName := e.Name()
-			paths = append(paths, dir+fileName)
-		}
-	}
-	return paths
-}
-
-func newSongs(paths []string) []*Song {
 	songs := []*Song{}
-	for _, p := range paths {
-		songs = append(songs, &Song{path: p})
-	}
+	_ = filepath.WalkDir(settings.dir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			fmt.Printf("[ERROR]: %v", err)
+			return err
+		}
+		if !d.IsDir() {
+			songs = append(songs, &Song{path: path})
+		}
+		return nil
+	})
 	return songs
 }
 
